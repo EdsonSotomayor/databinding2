987b8aa240455dce9a6c1ca586f8a515224c7bd8
Alumno <Alumno@PC04>
2019-09-24 09:41:31 -0500

    v1

---
+++ WpfApp1/WpfApp1/Empleado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        
+        public string Titulo { get; set; }
+
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "Edson Sotomayor",
+                Titulo = "Programador"
+            };
+            return emp;
+        }
+    }
+}
